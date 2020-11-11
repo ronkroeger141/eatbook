7ae08b488842bd7bf57ce417d55fb7ddf35c6d4f
ronkroeger141 <74055293+ronkroeger141@users.noreply.github.com>
2020-11-11 21:03:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,43 +1,9 @@
-<!DOCTYPE html> 
-<html> 
 
-<head> 
-	<title>Recipes Book</title> 
-	<style type="text/css"> 
-		html { 
-			overflow: auto; 
-		} 
-		
-		html, 
-		body, 
-		div, 
-		iframe { 
-			margin: 0px; 
-			padding: 0px; 
-			height: 100%; 
-			border: none; 
-		} 
-		
-		iframe { 
-			display: block; 
-			width: 100%; 
-			border: none; 
-			overflow-y: auto; 
-			overflow-x: hidden; 
-		} 
-	</style> 
-</head> 
-
-<body> 
-	<iframe src="https://www.joyfulhealthyeats.com/"
-			frameborder="0"
-			marginheight="0"
-			marginwidth="0"
-			width="100%"
-			height="100%"
-			scrolling="auto"> 
-</iframe> 
-
-</body> 
-
-</html> 
+<?php
+$url = $_GET['url'];
+if (isset ($url)) { // decode the url and display it in the iframe
+echo '<iframe ... src="'.urldecode($url).'"></iframe>';
+} else { // display a static page if no url parameter was received
+echo '<iframe ... src="https://www.foodiecrush.com/"></iframe>';
+}
+?>
